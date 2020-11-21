4b442fbe5c86ff90b375c521f0cb1ba13bc9987c
Nick Doelman <nick.doelman@outlook.com>
2020-11-21 14:22:14 -0500

    Updated JavaScript

--- flowscript.js
+++ flowscript.js
@@ -1,29 +1,48 @@
 <script type="text/javascript">
     function Reserve() { 
+        //Begin to build JSON string
         var requestjson = '{"requests": [';
+
+        //Get items from HTML table
         var records = document.getElementsByName('guidarray[]'); 
         var resvalues = document.getElementsByName('array[]'); 
-            
+        
+        //loop through each of the rows on the HTML table
         for (var i = 0; i < records.length; i++) { 
+
+            //get the guid of the record
             var a = records[i]; 
+            //get the request value
             var b = resvalues[i];
 
+            //continue to build JSON string
             requestjson = requestjson + '{"recordid": "' + a.value + '",';
             requestjson = requestjson + '"resvalue": ' + b.value + '}';
+
+            //if not at the end of records, add a comma to seperate records in JSON string
             if(i+1 < records.length) requestjson = requestjson + ',';
         } 
+        //close of the JSON string correctly
         requestjson = requestjson + ']}';
+
+        //display the JSON string on page so we can use it to build flow to accept request
         document.getElementById("par").innerHTML = requestjson; 
 
+        //instance a new XMLHttpRequest
         var req = new XMLHttpRequest();
-        var url = "https://prod-24.canadacentral.logic.azure.com:443/workflows/1a8850aa61034e86bcca931ae3518ee7/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=G1p-Q1Q9r5GeSqmiJ17qz2R-fS839Jkx_0X868xVswo";
 
+        //set to the URL generated from Power Automate
+        var url = "--URL from Power Automate--";
+
+        //send the request header and content as a POST
         req.open("POST", url, true);
         req.setRequestHeader('Content-Type', 'application/json');
         req.send(requestjson);
 
+        //Update the web page that the message has been sent
         document.getElementById("par").innerHTML = "Request Sent";
 
+        //Wait for a response to see if flow ran successfully and let end user know, will take a few seconds as flows run asychronously
         req.onreadystatechange = function() {
             if (req.readyState == XMLHttpRequest.DONE) {
               //alert(req.responseText);
